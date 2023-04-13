b64c33f9fb106669673381d9a2a2768ffcbe83a2
Nino <anthony@delcey.fr>
2023-04-13 12:48:40 +0200

    Initial commit

---
+++ app/src/main/java/fr/delcey/takepictureintentdemo/MainActivity.kt
@@ -0,0 +1,87 @@
+package fr.delcey.takepictureintentdemo
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import fr.delcey.takepictureintentdemo.databinding.MainActivityBinding
+import fr.delcey.takepictureintentdemo.utils.viewBinding
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding { MainActivityBinding.inflate(it) }
+
+    private var currentPhotoUri: Uri? = null
+
+    // region CONTRACTS
+    private val takePictureCallback = registerForActivityResult(ActivityResultContracts.TakePicture()) { successful ->
+        if (successful) {
+            Glide.with(binding.mainImageViewPhoto)
+                .load(currentPhotoUri)
+                .into(binding.mainImageViewPhoto)
+        }
+    }
+    // endregion CONTRACTS
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        // region CLASSIC
+        binding.mainButtonPicture.setOnClickListener {
+            currentPhotoUri = FileProvider.getUriForFile(
+                this,
+                BuildConfig.APPLICATION_ID + ".provider",
+                File.createTempFile(
+                    "JPEG_",
+                    ".jpg",
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                )
+            )
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                .putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri)
+
+            startActivityForResult(intent, 0)
+        }
+        // endregion CLASSIC
+
+        // region CONTRACTS
+        binding.mainButtonPictureWithContracts.setOnClickListener {
+            currentPhotoUri = FileProvider.getUriForFile(
+                this,
+                BuildConfig.APPLICATION_ID + ".provider",
+                File.createTempFile(
+                    "JPEG_",
+                    ".jpg",
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                )
+            )
+
+            takePictureCallback.launch(currentPhotoUri)
+        }
+        // endregion CONTRACTS
+    }
+
+    // region CLASSIC
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        @Suppress("DEPRECATION") // ActivityResultContracts are equally bad since they can't be registered after onCreate...
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            Glide.with(binding.mainImageViewPhoto)
+                .load(currentPhotoUri)
+                .into(binding.mainImageViewPhoto)
+        }
+    }
+    // endregion CLASSIC
+}
